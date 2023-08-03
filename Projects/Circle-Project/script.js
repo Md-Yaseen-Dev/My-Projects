3f112ac08bc5f074555a90a6502c03e22060cc38
mohammedyaseen2211 <ysn6040@gmail.com>
2023-08-03 18:01:53 +0530

    circle project updated

--- Projects/Circle-Project/script.js
+++ Projects/Circle-Project/script.js
@@ -1,35 +1,55 @@
 
 const bodyE1 = document.querySelector("body");
 
-const itemeE1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const itemeE1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-bodyE1.addEventListener("click", function (event) {
+let currentcircle = "null";
 
-    const xPos = event.offsetX;
-    const yPos = event.offsetY;
+function createCircle() {
     const divE1 = document.createElement("div");
-    divE1.style.left = xPos + "px";
-    divE1.style.top = yPos + "px";
-
-    const size = Math.random() * 100;
-    divE1.style.width = size + "px"
-    divE1.style.height = size + "px"
-    divE1.style.width = size 
+    const size = getRandomSize(10, 210);
+    // const size = Math.random() * 150;
+    divE1.style.width = size + "px";
+    divE1.style.height = size + "px";
 
     let color = "";
     for (let i = 0; i < 6; i++) {
 
         color += itemeE1[randomColor()];
     }
-    divE1.style.backgroundColor = `#${color}`
+    divE1.style.backgroundColor = `#${color}`;
     bodyE1.appendChild(divE1);
-    setTimeout(() => {
-        divE1.remove()
-    }, 2000)
+
+
+    timeout = setTimeout(() => {
+        divE1.remove();
+    }, 3000)
+    return divE1;
+
+
+}
+
+currentcircle = createCircle();
+document.addEventListener("click", (event) => {
+    currentcircle = createCircle();
+
+    currentcircle.style.left = event.clientX + "px";
+    currentcircle.style.top = event.clientY + "px";
+
 })
+
+document.addEventListener("mousemove", function (event) {
+    currentcircle.style.left = event.clientX + "px";
+    currentcircle.style.top = event.clientY + "px";
+
+})
+
 
 function randomColor() {
 
     return Math.floor(Math.random() * itemeE1.length)
 }
 
+function getRandomSize(min, max) {
+    return Math.random() *( max - min) + min
+}
